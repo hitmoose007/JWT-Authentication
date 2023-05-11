9a1d8cdf20300af6dd311851ea1376994fad14b8
Moosa Butt <moosabutt715@gmail.com>
2023-05-11 22:22:32 +0500

    env changed

--- authServer.js
+++ authServer.js
@@ -1,44 +1,42 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+//require env
+app.use(express.json());
 
-const express = require('express')
-const app = express()
-const jwt = require('jsonwebtoken')
+let refreshTokens = [];
 
-app.use(express.json())
+app.get("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, "refresh_secret", (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
+});
 
+app.get("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+});
 
-let refreshTokens = []
-
-app.get('/token', (req, res) => {
-    console.log('here')
-  const refreshToken = req.body.token
-  if (refreshToken == null) return res.sendStatus(401)
-  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
-    const accessToken = generateAccessToken({ name: user.name })
-    res.json({ accessToken: accessToken })
-  })
-})
-
-app.get('/logout', (req, res) => {
-  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-  res.sendStatus(204)
-})
-
-app.get('/login', (req, res) => {
+app.get("/login", (req, res) => {
   // Authenticate User
-console.log('here')
-  const username = req.body.username
-  const user = { name: username }
+  
+    const username = req.body.username;
+    const user = { name: username };
 
-  const accessToken = generateAccessToken(user)
-  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-  refreshTokens.push(refreshToken)
-  res.json({ accessToken: accessToken, refreshToken: refreshToken })
-})
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, "refresh_secret");
+    refreshTokens.push(refreshToken);
+  
+  res.json({ accessToken, refreshToken: refreshToken });
+});
 
 function generateAccessToken(user) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+  return jwt.sign(user, "access_secret", { expiresIn: "15s" });
 }
 
-app.listen(4000)
+app.listen(4000);
--- server.js
+++ server.js
@@ -24,7 +24,8 @@ function authenticateToken(req, res, next) {
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  
+  jwt.verify(token, "access_token_secret", (err, user) => {
     console.log(err);
     if (err) return res.sendStatus(403);
     req.user = user;
